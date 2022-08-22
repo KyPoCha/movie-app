c9b41f0b9feaea5368c765ea9632963a43c5ae42
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-08-22 19:23:08 +0200

    Update for styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 function App(){
     return (
-        <div className="App">
+        <React.Fragment>
             <Header/>
-        </div>
+            <Footer/>
+        </React.Fragment>
     );
 }
 
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,6 @@
 function Header(){
     return (
-        <nav>
+        <nav className="pink darken-4">
             <div className="nav-wrapper">
                 <a href="#" className="brand-movies">Movies</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
