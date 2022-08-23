026f66738c42e89241298e24d98f937935dc7b7e
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-08-23 19:48:55 +0200

    Create search panel and add some styles to each component on the page

---
+++ src/components/Search.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default class Search extends React.Component{
+
+    state = {
+        search: "",
+        type: "",
+    }
+
+    handleKey = (event)=>{
+        if(event.key === "Enter"){
+            this.props.searchMovies(this.state.search);
+        }
+    }
+
+    handleFilter = (event)=>{
+        this.setState(()=>({type: event.target.dataset.type}),()=>{
+            this.props.searchMovies(this.state.search, this.state.type);
+        });
+    }
+
+    render() {
+        return(
+            <div className="row">
+                        <input
+                            className="validate grey-text text-lighten-5"
+                            id="search_inline"
+                            type="search"
+                            placeholder="Search"
+                            value={this.state.search}
+                            onChange={(event)=>this.setState({search: event.target.value})}
+                            onKeyDown={this.handleKey}
+                        />
+                <button className="waves-effect waves-light btn pink darken-4 grey-text text-lighten-5 btn-search"
+                    onClick={()=>{
+                        this.props.searchMovies(this.state.search,this.state.type);
+                    }}
+                >Search</button>
+                <div>
+                    <label>
+                        <input className="with-gap" name="type" type="radio" data-type="all" onChange={this.handleFilter} checked={this.state.type === "all"}/>
+                        <span>All</span>
+                    </label>
+                    <label>
+                        <input className="with-gap" name="type" type="radio" data-type="movie" onChange={this.handleFilter} checked={this.state.type === "movie"}/>
+                        <span>Movies only</span>
+                    </label>
+                    <label>
+                        <input className="with-gap" name="type" type="radio" data-type="series" onChange={this.handleFilter} checked={this.state.type === "series"}/>
+                        <span>Series only</span>
+                    </label>
+                </div>
+            </div>
+        );
+    }
+}
